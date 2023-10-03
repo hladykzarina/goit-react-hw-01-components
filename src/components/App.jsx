22ad817e659d434473cd264ef658ecc5a3e3636a
bogunenko-daryna <124713486+hladykzarina@users.noreply.github.com>
2023-10-03 17:58:22 +0200

    friends

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,15 @@
-import user from './Profile/user.json';
+//import user from './Profile/user.json';
 import Profile from './Profile/Profile.jsx';
 
-import data from '../components/Statistics/data.json';
-import Statistics from '../components/Statistics/Statistics';
+//import data from '../components/Statistics/data.json';
+import Statistics from './Statistics/Statistics.jsx';
 
-import friends from '../components/FriendList/friends.json';
+//import friends from '../components/FriendList/friends.json';
 import FriendList from '../components/FriendList/FriendList';
+
+const user = { user };
+const data = { data };
+const friends = { friends };
 
 export const App = () => {
   return (
