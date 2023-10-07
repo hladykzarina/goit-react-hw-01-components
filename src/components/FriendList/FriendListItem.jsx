09d646c7314dda2e47ca352f854478e650f6237e
bogunenko-daryna <124713486+hladykzarina@users.noreply.github.com>
2023-10-07 10:47:54 +0200

    avatars

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -10,7 +10,7 @@ export const FriendListItem = ({ avatar, name, isOnline }) => {
           [css.red]: isOnline === false,
         })}
       ></span>
-      <img className={css.avatar} scr={avatar} alt={name} width="48" />
+      <img className={css.avatar} src={avatar} alt={name} width="48" />
       <p className={css.name}>{name}</p>
     </li>
   );
